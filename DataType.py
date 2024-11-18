2f51f24796d2be370e6125e7289f1c141850137f
Tawhidul Islam <tawhidulislam3482@gmail.com>
2024-11-18 17:03:39 +0600

    dataType

---
+++ DataType.py
@@ -0,0 +1,62 @@
+# String type data 
+
+StringType = 'Hi My name is Tawhidul Islam'
+
+print(StringType)
+
+
+# int type data 
+
+IntType = 25
+
+print(IntType)
+print(type(IntType))
+
+# floating type 
+FloatType = 20.9
+print(type(FloatType))
+
+# complex type 
+
+ComplexType = 15565j
+print(type(ComplexType))
+
+
+# str data type 
+
+myName ='Tawhidul Islam'
+print("My name is"+ ' '+ myName)
+
+# boolean type  
+
+booleanType = True
+print(booleanType) 
+print(type(booleanType)) 
+
+x =8 
+y= 8
+print(x >= y)
+print(x == y)
+
+# string formatting 
+
+num1 = 25 
+num2 = 14
+print(f"add the number  {num1 + num2}")
+print(f"add the number  {num1 - num2}")
+print(f"add the number  {50 * 15}")
+print(f"my name is  {myName}")
+
+# Binary types 
+# bytes type 
+listOfNumber = [1,12,51,25,2,45,45,]
+b = bytes(listOfNumber)
+print(type(b))
+
+
+# bytearray 
+listOfNumber = [1,12,51,25,2,45,45,]
+b1 = bytearray(listOfNumber)
+b1[0]=10
+print(b1[0])
+print(type(b1))
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("hello Saikat")
 # print("hello Tawhid")
+
+'''hi how are you?
+i am cool 
+what about         you?
+'''
+
+name = 25
+ami = 18
+
+print(name + ami)
