5e4211356fae20942dba023bd98fde879bfc451c
Tawhidul Islam <tawhidulislam3482@gmail.com>
2024-11-24 16:19:57 +0600

    sort list

---
+++ ListComprehension.py
@@ -0,0 +1,22 @@
+
+# list = ['ami','tumi','bhai','bhai']
+
+
+# comprehension 
+'''step1 = []
+    step2 = what i do
+    step3 = for loop 
+    
+'''
+
+
+list1 = [1,6,16,15,45]
+
+squares = [li*2 for li in list1]
+print(squares)
+
+ami  = [i**2 for i in list1]
+print(ami)
+
+tumi = [b+5 for b in list1]
+print(tumi)
---
+++ SortLists.py
@@ -0,0 +1,24 @@
+# list=[3.6,8,9,4,6,4,7,]
+# list= ['ami','Tumi','saikat']
+
+# list.sort()
+# list.sort(reverse=True)
+# # list.sort(key=str.lower)
+
+# # print(list)
+
+# # // reverse method
+# list.reverse()
+
+
+# copy list 
+list1= ['ami','Tumi','saikat']
+
+list2= list1.copy()
+
+# print(list2)
+
+myList = list(list1)
+print(myList)
+print (((78-25)+8) /10)
+
